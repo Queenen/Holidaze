26da973517a10c00352afde00d2ed84bee64a5c1
Annette B <114480324+Queenen@users.noreply.github.com>
2024-04-26 13:21:59 +0200

    Adding routing system

---
+++ src/AppRouter.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import VenuesPage from "./pages/VenuesPage";
+import VenuePage from "./pages/VenuePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <Layout>
+            <ProfilePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/venues"
+        element={
+          <Layout>
+            <VenuesPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/venue"
+        element={
+          <Layout>
+            <VenuePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <Layout>
+            <NotFoundPage />
+          </Layout>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default AppRouter;
