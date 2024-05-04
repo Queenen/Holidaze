1b4121c92d41f1e73ce271d53fe792775191878d
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-04 11:36:10 +0200

    fixing navbar bug

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -44,52 +44,55 @@ const Header = () => {
   };
 
   return (
-    <header
-      className={`d-flex align-items-center p-3 px-md-5 ${styles.header}`}
-    >
-      <Navbar expand="md" collapseOnSelect className="w-100">
-        <Container className={`d-flex p-0 gap-4 ${styles.container}`}>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="Holidaze logo" className={styles.logo} />
-          </Navbar.Brand>
-          <button
-            className="btn order-md-1 ms-auto"
-            onClick={handleSignInOutClick}
-          >
-            <FontAwesomeIcon
-              className="me-3"
-              icon={isLoggedIn ? faSignOutAlt : faSignInAlt}
-            />
-            {isLoggedIn ? "Sign Out" : "Sign In"}
-          </button>
-          <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            className={styles.toggleBtn}
-          >
-            <FontAwesomeIcon icon={faBars} />
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav
-              className={`me-auto order-md-0 text-center ${styles.navLinks}`}
+    <>
+      <div className={styles.space}></div>
+      <header
+        className={`d-flex align-items-center p-3 px-md-5 ${styles.header}`}
+      >
+        <Navbar expand="md" collapseOnSelect className="w-100">
+          <Container className={`d-flex p-0 gap-4 ${styles.container}`}>
+            <Navbar.Brand href="/">
+              <img src={logo} alt="Holidaze logo" className={styles.logo} />
+            </Navbar.Brand>
+            <button
+              className="btn order-md-1 ms-auto"
+              onClick={handleSignInOutClick}
             >
-              <Nav.Link href="/venues">All Venues</Nav.Link>
-              {isLoggedIn && <Nav.Link href="/profile">Profile</Nav.Link>}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Modal show={showModal} onHide={handleCloseModal} centered>
-        {showSignUp ? (
-          <SignUp closeModal={handleCloseModal} onToggleAuth={toggleSignUp} />
-        ) : (
-          <SignIn
-            closeModal={handleCloseModal}
-            onToggleAuth={toggleSignUp}
-            setIsLoggedIn={setIsLoggedIn}
-          />
-        )}
-      </Modal>
-    </header>
+              <FontAwesomeIcon
+                className="me-3"
+                icon={isLoggedIn ? faSignOutAlt : faSignInAlt}
+              />
+              {isLoggedIn ? "Sign Out" : "Sign In"}
+            </button>
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              className={styles.toggleBtn}
+            >
+              <FontAwesomeIcon icon={faBars} />
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav
+                className={`me-auto order-md-0 text-center ${styles.navLinks}`}
+              >
+                <Nav.Link href="/venues">All Venues</Nav.Link>
+                {isLoggedIn && <Nav.Link href="/profile">Profile</Nav.Link>}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Modal show={showModal} onHide={handleCloseModal} centered>
+          {showSignUp ? (
+            <SignUp closeModal={handleCloseModal} onToggleAuth={toggleSignUp} />
+          ) : (
+            <SignIn
+              closeModal={handleCloseModal}
+              onToggleAuth={toggleSignUp}
+              setIsLoggedIn={setIsLoggedIn}
+            />
+          )}
+        </Modal>
+      </header>
+    </>
   );
 };
 
