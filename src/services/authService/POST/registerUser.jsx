d9adc0c510835dd0dcbd204563fa795411c24141
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-10 22:20:08 +0200

    Create registerUser.jsx

---
+++ src/services/authService/POST/registerUser.jsx
@@ -0,0 +1,27 @@
+import { NOROFF_API_URL } from "../../config";
+
+// Function to register a user
+export async function registerUser(userData) {
+  try {
+    const response = await fetch(`${NOROFF_API_URL}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Failed to register user: ${errorData.message}`);
+    }
+
+    // Log the data sent to the server
+    console.log("Data sent to server:", userData);
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+}
