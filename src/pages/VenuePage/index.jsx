b56e2734bd048f90a3092afd911d405a11e59ea8
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-10 22:23:02 +0200

    Update index.jsx

--- src/pages/VenuePage/index.jsx
+++ src/pages/VenuePage/index.jsx
@@ -1,12 +1,18 @@
 import { VenueCarousel } from "./components/Carousel";
 import VenueInfo from "./components/VenueInfo";
+import Booking from "./components/Booking";
+import { VenueProvider } from "../../context/VenueContext";
+import { useSearchParams } from "react-router-dom";
 
 const VenuePage = () => {
+  const [searchParams] = useSearchParams();
+  const venueId = searchParams.get("id");
   return (
-    <>
+    <VenueProvider venueId={venueId}>
       <VenueCarousel />
       <VenueInfo />
-    </>
+      <Booking />
+    </VenueProvider>
   );
 };
 
