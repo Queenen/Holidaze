5f198460b6b38ad071483ed61f8c2d19fade4967
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-10 22:20:12 +0200

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -4,3 +4,6 @@ export const NOROFF_API_URL = "https://v2.api.noroff.dev";
 // Function to get the venue URL
 export const getVenueUrl = (venueId) =>
   `${NOROFF_API_URL}/holidaze/venues/${venueId}`;
+
+// URL for booking a venue
+export const bookVenueUrl = `${NOROFF_API_URL}/holidaze/bookings`;
