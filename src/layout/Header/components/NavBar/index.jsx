1b069fa8d0589075b494a8e66ebb5b8ed63dce83
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-11 00:56:42 +0200

    created NavBar component

---
+++ src/layout/Header/components/NavBar/index.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSignInAlt,
+  faSignOutAlt,
+  faCirclePlus,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link, NavLink } from "react-router-dom";
+import { useUserContext } from "../../../../context/UserContext";
+import logo from "../../../../asset/logo/Holidaze_Logo.png";
+import styles from "./NavBar.module.css";
+
+function NavBar({ handleSignInOutClick, toggleAddVenueModal }) {
+  const { role, isLoggedIn } = useUserContext();
+  const userName = sessionStorage.getItem("userName");
+  const [expanded, setExpanded] = useState(false);
+  const navbarRef = useRef(null);
+
+  const toggleNavbar = () => setExpanded(!expanded);
+  const closeNavbar = () => setExpanded(false);
+
+  // Handle clicking outside the navbar to close it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+        closeNavbar();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [navbarRef]);
+
+  return (
+    <Navbar
+      expand="md"
+      collapseOnSelect
+      expanded={expanded}
+      className="w-100"
+      ref={navbarRef}
+    >
+      <Container className={`d-flex p-0 gap-4 ${styles.container}`}>
+        <Navbar.Brand as={Link} to="/" onClick={closeNavbar}>
+          <img src={logo} alt="Holidaze logo" className={styles.logo} />
+        </Navbar.Brand>
+        <button
+          className="btn order-md-1 ms-auto"
+          onClick={() => {
+            handleSignInOutClick();
+            closeNavbar();
+          }}
+        >
+          <FontAwesomeIcon
+            icon={isLoggedIn ? faSignOutAlt : faSignInAlt}
+            className="me-3"
+          />
+          {isLoggedIn ? "Sign Out" : "Sign In"}
+        </button>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className={styles.toggleBtn}
+          onClick={toggleNavbar}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav
+            className={`me-auto order-md-0 text-center gap-md-4 ${styles.navLinks}`}
+            onClick={closeNavbar}
+          >
+            {role === "venue manager" && (
+              <button
+                className="nav-link p-0 border-0 bg-transparent py-2 p-md-2"
+                onClick={() => {
+                  toggleAddVenueModal();
+                  closeNavbar();
+                }}
+              >
+                <FontAwesomeIcon icon={faCirclePlus} className="me-2" />
+                Add Venue
+              </button>
+            )}
+            <Nav.Link as={NavLink} to="/venues">
+              All Venues
+            </Nav.Link>
+            {isLoggedIn && (
+              <Nav.Link as={NavLink} to={`/profile?name=${userName}`}>
+                Profile
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
