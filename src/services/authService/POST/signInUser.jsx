87aac899ca3b162e67dde206b71d9773819f088b
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-10 22:20:10 +0200

    Create signInUser.jsx

---
+++ src/services/authService/POST/signInUser.jsx
@@ -0,0 +1,29 @@
+import { NOROFF_API_URL } from "../../config";
+
+// Function to sign in user
+export async function loginUser(credentials) {
+  try {
+    const response = await fetch(`${NOROFF_API_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Failed to sign in: ${errorData.message}`);
+    }
+
+    // Log the credentials sent to the server
+    console.log("Credentials sent to server:", credentials);
+
+    const data = await response.json();
+    sessionStorage.setItem("accessToken", data.data.accessToken);
+    return data;
+  } catch (error) {
+    console.error("Error logging in:", error);
+    throw error;
+  }
+}
