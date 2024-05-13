6be96643c6990700cf658c5f39d18c8a751ccd77
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-13 14:15:34 +0200

    Added context files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./context/UserContext";
+import { UserStatusProvider } from "./context/UserStatus";
+import { UserRoleProvider } from "./context/UserRole";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserStatusProvider>
+        <UserRoleProvider>
+          <App />
+        </UserRoleProvider>
+      </UserStatusProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 reportWebVitals();
