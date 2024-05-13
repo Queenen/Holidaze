79194e3dbfd0e1cc65cfada1c4384ed88d6fbfe8
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-13 14:16:26 +0200

    Update index.jsx

--- src/pages/VenuePage/components/VenueInfo/index.jsx
+++ src/pages/VenuePage/components/VenueInfo/index.jsx
@@ -23,29 +23,30 @@ function VenueInfo() {
 
   return (
     <section
-      className={`d-flex justify-content-center p-5 ${styles.infoContainer}`}
+      className={`d-flex justify-content-center p-5 col-md-6 col-lg-8 order-1 ${styles.infoContainer}`}
     >
       {venue ? (
         <div className={`d-flex flex-column gap-3 w-100 ${styles.venueInfo}`}>
-          <h1 className="mb-3">
-            <TextTruncate text={venue.name} maxLength={15} />
-          </h1>
-          <div className="fst-italic d-flex gap-2">
-            {" "}
+          <TextTruncate text={venue.name} maxLength={22} className="fs-1" />
+          <div className="fst-italic d-flex gap-2 align-items-center">
             <span>
               <FontAwesomeIcon icon={faLocationDot} />
             </span>
             {venue.location?.address ? (
-              <TextTruncate text={venue.location.address} maxLength={15} />
+              <TextTruncate text={venue.location.address} maxLength={22} />
             ) : (
               "Unknown location"
             )}
           </div>
           <p>
             {" "}
-            <TextTruncate text={venue.description} maxLength={30} />
+            <TextTruncate
+              text={venue.description}
+              maxLength={300}
+              className="lh-lg my-3"
+            />
           </p>
-          <p>
+          <p className="my-3">
             <span className="fw-bold">Price:</span> $ {venue.price} per day
           </p>
         </div>
