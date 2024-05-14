a7a7b09bc3ad2e8c6c49b4eddcde8d85ede66d3b
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-14 12:41:28 +0200

    Update index.jsx

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,10 +3,7 @@ import styles from "./Button.module.css";
 function Button({ children, onClick }) {
   return (
     <>
-      <button
-        onClick={onClick}
-        className={`btn rounded-5 text-white fw-bold text-uppercase px-4 ${styles.button}`}
-      >
+      <button onClick={onClick} className={styles.button}>
         {children}
       </button>
     </>
