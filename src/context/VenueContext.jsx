578523aac574faa9a2f3f5e4494ddbd09d8f2c69
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-18 20:06:50 +0200

    Update VenueContext.jsx

--- src/context/VenueContext.jsx
+++ src/context/VenueContext.jsx
@@ -6,7 +6,6 @@ const VenueContext = createContext();
 export const useVenue = () => useContext(VenueContext);
 
 export const VenueProvider = ({ venueId, children }) => {
-  // Use the custom hook to manage venue data fetching
   const { data: venue, loading, error } = useFetchVenue(venueId);
 
   // Provide the fetched data through the VenueContext
