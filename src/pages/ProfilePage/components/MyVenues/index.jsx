b176b9436166c9058bf0739a0747e2af653fe86b
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-21 21:13:03 +0200

    Update index.jsx

--- src/pages/ProfilePage/components/MyVenues/index.jsx
+++ src/pages/ProfilePage/components/MyVenues/index.jsx
@@ -1,31 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { Carousel } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUsers,
-  faStarHalfStroke,
-  faUtensils,
-  faCarSide,
-  faPaw,
-  faWifi,
-} from "@fortawesome/free-solid-svg-icons";
-import Button from "../../../../components/Button";
-import TextTruncate from "../../../../components/TextTruncate";
 import styles from "./MyVenues.module.css";
-import { Link } from "react-router-dom";
+import VenueCarousel from "../../../../components/Carousel";
+import EditVenue from "../EditVenue";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 const MyVenues = ({ user, showEditButton = true }) => {
-  const venues = user.venues;
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedVenue, setSelectedVenue] = useState(null);
 
   useEffect(() => {
-    if (venues.length === 0) {
-      setIsVisible(false);
-    }
-  }, [venues.length]);
+    setIsVisible(user.venues.length > 0);
+  }, [user.venues.length]);
+
+  const fetchUserVenues = async () => {
+    return user.venues.map((venue) => ({
+      id: venue.id,
+      ...venue,
+    }));
+  };
 
   function toggleVenues() {
     setIsVisible(!isVisible);
+  }
+
+  function handleEditVenue(e, venue) {
+    e.preventDefault();
+    e.stopPropagation();
+    setSelectedVenue(venue);
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+    setSelectedVenue(null);
   }
 
   return (
@@ -33,98 +42,34 @@ const MyVenues = ({ user, showEditButton = true }) => {
       <button className={styles.showVenues} onClick={toggleVenues}>
         {isVisible ? "Hide my venues" : "Show my venues"}
       </button>
-      <Carousel
-        className={`${styles.myVenuesContent} ${
-          isVisible ? "" : styles.hidden
-        }`}
-      >
-        {venues.length === 0 && (
-          <div className={styles.emptyContent}>
-            You haven't added any venues yet.
-          </div>
-        )}
-        {venues &&
-          venues.map((venue) => (
-            <Carousel.Item
-              key={venue.id}
-              className={`position-relative ${styles.carouselItem}`}
+      {isVisible && (
+        <>
+          {user.venues.length > 0 ? (
+            <VenueCarousel
+              fetchFunction={fetchUserVenues}
+              showEditButton={showEditButton}
+              height="25rem"
+              onEditClick={handleEditVenue}
             >
-              <Link to={`/venue?id=${venue.id}`}>
-                {venue.media && venue.media.length > 0 && (
-                  <div className={styles.backgroundImageContainer}>
-                    <img
-                      className="d-block w-100"
-                      src={venue.media[0].url}
-                      alt={venue.media[0].alt || venue.name}
-                    />
-                    <div className={styles.overlay}></div>
-                  </div>
-                )}
-                <div className="d-flex justify-content-between position-absolute top-0 p-4 w-100">
-                  <div className="d-flex gap-3 align-items-center">
-                    <FontAwesomeIcon
-                      icon={faStarHalfStroke}
-                      color="white"
-                      className={styles.icon}
-                    />
-                    <p className="fw-bold text-white">{venue.rating}</p>
-                  </div>
-                  <div className="d-flex gap-3 align-items-center">
-                    <FontAwesomeIcon
-                      icon={faUsers}
-                      color="white"
-                      className={styles.icon}
-                    />
-                    <p className="fw-bold text-white">{venue.maxGuests}</p>
-                  </div>
-                </div>
-                <Carousel.Caption>
-                  {showEditButton && (
-                    <div className={styles.editBtn}>
-                      <Button>Edit Venue</Button>
-                    </div>
-                  )}
-                  <h2 className="d-flex justify-content-center fs-">
-                    <TextTruncate text={venue.name} maxLength={30} />
-                  </h2>
-                  <p className="d-flex justify-content-center">
-                    <TextTruncate text={venue.description} maxLength={30} />
-                  </p>
-                  <div className="facilities d-flex gap-4 justify-content-center my-3">
-                    {venue.meta.wifi && (
-                      <FontAwesomeIcon
-                        icon={faWifi}
-                        color="white"
-                        className={styles.icon}
-                      />
-                    )}
-                    {venue.meta.parking && (
-                      <FontAwesomeIcon
-                        icon={faCarSide}
-                        color="white"
-                        className={styles.icon}
-                      />
-                    )}
-                    {venue.meta.breakfast && (
-                      <FontAwesomeIcon
-                        icon={faUtensils}
-                        color="white"
-                        className={styles.icon}
-                      />
-                    )}
-                    {venue.meta.pets && (
-                      <FontAwesomeIcon
-                        icon={faPaw}
-                        color="white"
-                        className={styles.icon}
-                      />
-                    )}
-                  </div>
-                </Carousel.Caption>
-              </Link>
-            </Carousel.Item>
-          ))}
-      </Carousel>
+              Edit Venue
+            </VenueCarousel>
+          ) : (
+            <div className={styles.emptyContent}>
+              <p className="text-danger">
+                <FontAwesomeIcon
+                  icon={faCircleExclamation}
+                  color="red"
+                  className="me-1"
+                />{" "}
+                You haven't added any venues yet.
+              </p>
+            </div>
+          )}
+        </>
+      )}
+      {showModal && (
+        <EditVenue closeModal={closeModal} venueData={selectedVenue} />
+      )}
     </section>
   );
 };
