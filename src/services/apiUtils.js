dee4e59046b2a0c1857fc5a662ca0814b86e7999
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-21 21:26:02 +0200

    deleted unused files

--- src/services/apiUtils.js
+++
@@ -1,8 +0,0 @@
-import { NOROFF_API_URL } from "./config";
-
-export function getVenueUrl(venueId) {
-  if (!venueId || typeof venueId !== "string") {
-    throw new Error("A valid venue ID is required");
-  }
-  return `${NOROFF_API_URL}/holidaze/venues/${venueId}`;
-}
