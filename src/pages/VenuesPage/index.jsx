cdce65ae9740f8a1d7baa52ba2246bc7d24a1736
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-21 21:12:49 +0200

    Update index.jsx

--- src/pages/VenuesPage/index.jsx
+++ src/pages/VenuesPage/index.jsx
@@ -2,8 +2,10 @@
 import HeroSection from "./components/HeroSection";
 import AllVenues from "./components/AllVenues";
 import { FilterProvider } from "../../context/FilterContext";
+import useDocumentTitle from "../../hooks/useDocumentTitle";
 
 function VenuesPage() {
+  useDocumentTitle("Holidaze | Venues");
   return (
     <FilterProvider>
       <HeroSection />
