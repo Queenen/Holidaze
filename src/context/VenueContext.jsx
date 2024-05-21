918ba8f2caff2b62313384224d88a84f86a964e6
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-21 21:13:33 +0200

    Update VenueContext.jsx

--- src/context/VenueContext.jsx
+++ src/context/VenueContext.jsx
@@ -6,9 +6,8 @@ const VenueContext = createContext();
 export const useVenue = () => useContext(VenueContext);
 
 export const VenueProvider = ({ venueId, children }) => {
-  const { data: venue, loading, error } = useFetchVenue(venueId);
+  const { venue, loading, error } = useFetchVenue(venueId);
 
-  // Provide the fetched data through the VenueContext
   return (
     <VenueContext.Provider value={{ venue, loading, error }}>
       {children}
