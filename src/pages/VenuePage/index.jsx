7545d73e68ff3b6db9af4ab15a6c55547024dec1
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-21 21:12:51 +0200

    Update index.jsx

--- src/pages/VenuePage/index.jsx
+++ src/pages/VenuePage/index.jsx
@@ -1,22 +1,34 @@
-import { VenueCarousel } from "./components/Carousel";
+import React from "react";
 import VenueInfo from "./components/VenueInfo";
 import Booking from "./components/Booking";
-import { VenueProvider } from "../../context/VenueContext";
 import { useSearchParams } from "react-router-dom";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
+import VenueCarousel from "./components/Carousel";
+import { useFetchVenue } from "../../services/authService/GET/fetchVenueById";
+import Loader from "../../components/Loader";
 
 const VenuePage = () => {
   useDocumentTitle(`Holidaze | Venue`);
   const [searchParams] = useSearchParams();
   const venueId = searchParams.get("id");
+
+  const { venue, loading, error } = useFetchVenue(venueId);
+
+  if (loading) return <Loader />;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <VenueProvider venueId={venueId}>
-      <VenueCarousel />
-      <div className="d-md-flex">
-        <VenueInfo />
-        <Booking />
-      </div>
-    </VenueProvider>
+    <div>
+      {venue && (
+        <>
+          <VenueCarousel venue={venue.data} />
+          <div className="d-md-flex">
+            <VenueInfo venue={venue.data} loading={loading} error={error} />
+            <Booking venue={venue} loading={loading} error={error} />
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
