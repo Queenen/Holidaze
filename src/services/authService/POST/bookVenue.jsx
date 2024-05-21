3b4d0be6a021d3db379b27e695383e79417c1e6e
Annette B <114480324+Queenen@users.noreply.github.com>
2024-05-21 21:12:36 +0200

    Update bookVenue.jsx

--- src/services/authService/POST/bookVenue.jsx
+++ src/services/authService/POST/bookVenue.jsx
@@ -1,29 +1,48 @@
 import { bookVenueUrl } from "../../config";
+import createApiKey from "../../apiAuth";
 
 // Function to book venue
-export async function bookVenue(dateFrom, dateTo, guests, venueId) {
+
+async function ensureApiKey() {
+  if (!sessionStorage.getItem("apiKey")) {
+    await createApiKey();
+  }
+  return sessionStorage.getItem("apiKey");
+}
+
+export async function bookVenue({ dateFrom, dateTo, guests, venueId }) {
   const url = bookVenueUrl;
   const token = sessionStorage.getItem("accessToken");
-  const apiKey = sessionStorage.getItem("apiKey");
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-        "X-Noroff-API-Key": apiKey,
-      },
-      body: JSON.stringify({ dateFrom, dateTo, guests, venueId }),
-    });
+  if (!token) {
+    console.error("No access token available.");
+    throw new Error(
+      "You're not authorized to handle this request. Please sign in and try again."
+    );
+  }
 
-    if (!response.ok) {
-      throw new Error(`Failed to book venue with status: ${response.status}`);
+  const apiKey = await ensureApiKey();
+
+  if (token && apiKey) {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          "X-Noroff-API-Key": apiKey,
+        },
+        body: JSON.stringify({ dateFrom, dateTo, guests, venueId }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to book venue with status: ${response.status}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Error booking venue:", error);
+      throw error;
     }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Error booking venue:", error);
-    throw error;
   }
 }
